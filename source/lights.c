4499342296ae7119849f9f16d57c440aa7472fd6
David Chen <chen749@usc.edu>
2017-08-06 00:40:32 -0700

    delay

--- source/lights.c
+++ source/lights.c
@@ -30,6 +30,12 @@ void lightUpMyWorld()
 	char length;
 	char message[] = "OK Mika!";
 	char messageBuffer[30];
+
+	length = formatString(message,messageBuffer,0);
+	print(messageBuffer,length);
+	timerWaitMs(2500000);
+
+
 	setGpioFunc(9,1);
 	setGpioFunc(17,1);
 	setGpioFunc(23,1);
@@ -43,10 +49,6 @@ void lightUpMyWorld()
 	setGpioFunc(8,1);
 	setGpioFunc(7,1);
 	setGpioFunc(24,1);
-
-	length = formatString(message,messageBuffer,0);
-	print(messageBuffer,length);
-	timerWaitMs(2500000);
 
 	lightNine();
 	timerWaitMs(2500000);
